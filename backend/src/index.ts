49360827f5c1724f30610a1c25dce38684e3f3d4
Mao072 <ryanwu0517@gmail.com>
2024-09-03 22:37:30 +0800

    建立分支

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { v4 as uuidv4 } from 'uuid';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const prisma = new PrismaClient();
+
+const app = express()
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
