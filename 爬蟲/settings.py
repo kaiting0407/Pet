c53d585216455f0041105cdaffa79e8c74ccb762
kaiting0407 <mike26593643@gmail.com>
2024-09-05 22:02:31 +0800

    spider

---
+++ 爬蟲/settings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @File    : settings.py
+# @Author  : AaronJny
+# @Time    : 2019/12/16
+# @Desc    :
+
+
+# ##########爬虫############
+
+# 图片类别和搜索关键词的映射关系
+IMAGE_CLASS_KEYWORD_MAP = {
+    'glassland': '草地',
+    'beach': '海灘',
+    'forest': '森林'
+}
+# 图片保存根目录
+IMAGES_ROOT = './images'
+# 爬虫每个类别下载多少页图片
+SPIDER_DOWNLOAD_PAGES = 10
+
+# #########数据###########
+
+#
